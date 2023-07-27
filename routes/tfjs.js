7777341463ad2f38455c5932a0ee9fa27db31e48
baekggum <c3man4@naver.com>
2023-07-27 15:28:57 +0900

    4일차 tfjs

--- routes/tfjs.js
+++ routes/tfjs.js
@@ -1,6 +1,8 @@
 const express     = require('express');
 const router      = express.Router();
 
+const tf = require('@tensorflow/tfjs');
+
 router.get('/regression1', function(req, res) {
     res.render('tfjs/regression1');
 });
@@ -10,7 +12,15 @@ router.get('/regression2', function(req, res) {
 router.post('/regression2', async function(req, res) {
 		//모델 로드
 		//예측
-    res.send('예측결과');
+    let model = await tf.loadLayersModel('https://raw.githubusercontent.com/baekggum/2023vac_nodejs/main/reg_model.json');
+    model.summary();
+    const data = req.body.data;
+    let testX = data.split(',').map(function(x){
+        return parseInt(x);
+    });
+    testX = tf.tensor(testX, [testX.length]);
+    const preds = model.predict(testX).arraySync();
+    res.send(preds);
 });
 
 module.exports = router;
