ea7f7d6028d7ee9fa7bb318244fe49012c368897
baekggum <c3man4@naver.com>
2023-07-28 11:56:25 +0900

    5일차 TL

--- routes/tfjs.js
+++ routes/tfjs.js
@@ -23,4 +23,31 @@ router.post('/regression2', async function(req, res) {
     res.send(preds);
 });
 
+//inference #1 tensorflow-hub 업로드한 이미지 분류
+router.get('/inference1', async function(req, res) {
+    res.render('tfjs/inference1');
+});
+//inference #2 mobilenet.js 업로드한 이미지 분류
+router.get('/inference2', async function(req, res) {
+    res.render('tfjs/inference2');
+});
+//inference #3 mobilenet.js 웹캠 분류
+router.get('/inference3', async function(req, res) {
+    res.render('tfjs/inference3');
+});
+//inference #4 ssd.js 웹캠 객체 탐지
+router.get('/inference4', async function(req, res) {
+    res.render('tfjs/inference4');
+});
+
+// transfer #1 yolov5 웹캠
+router.get('/transfer1', async function(req, res) {
+    res.render('tfjs/transfer1');
+});
+
+// transfer #2 tl모델 웹캠
+router.get('/transfer2', async function(req, res) {
+    res.render('tfjs/transfer2');
+});
+
 module.exports = router;
